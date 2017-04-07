431062fd57365da961c19684d555684091408415
Yuki Sumida <y.sumida.0201@gmail.com>
2017-04-07 18:44:31 +0900

    NSCodingを使って自作クラスをUserDefaultsに保存する

---
+++ NSCodingPlayground.playground/Contents.swift
@@ -0,0 +1,45 @@
+//: Playground - noun: a place where people can play
+
+import Foundation
+
+class Hoge: NSObject, NSCoding {
+    var id: Int = 0
+    var name: String = ""
+    
+    init(id: Int, name: String) {
+        super.init()
+        
+        self.id = id
+        self.name = name
+    }
+    
+    func encode(with aCoder: NSCoder) {
+        aCoder.encode(self.id, forKey: "id")
+        aCoder.encode(self.name, forKey: "name")
+    }
+    
+    required init(coder aDecoder: NSCoder) {
+        super.init()
+        
+        self.id = aDecoder.decodeInteger(forKey: "id")
+        self.name = aDecoder.decodeObject(forKey: "name") as! String
+    }
+}
+
+let hoge = Hoge(id: 1, name: "hogehoge")
+
+let sharedInstance = Foundation.UserDefaults.standard
+
+let object = NSKeyedArchiver.archivedData(withRootObject: hoge)
+sharedInstance.set(object, forKey: "hoge")
+sharedInstance.synchronize()
+
+
+if let data = sharedInstance.object(forKey: "hoge") as? Data {
+    let result = NSKeyedUnarchiver.unarchiveObject(with: data) as! Hoge
+    dump(result)
+}
+
+
+
+
