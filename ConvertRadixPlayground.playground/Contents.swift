ef3d3031a93005a65d7e4ee631687ea8104946d2
Yuki Sumida <y.sumida.0201@gmail.com>
2017-04-20 19:21:34 +0900

    Int -> Stringで基数変換して表示

---
+++ ConvertRadixPlayground.playground/Contents.swift
@@ -0,0 +1,9 @@
+//: Playground - noun: a place where people can play
+import Foundation
+
+let R = 255
+let G = 0
+let B = 16
+let r: String = String(R, radix: 16)
+let g: String = String(G, radix: 16)
+let b: String = String(B, radix: 16)
