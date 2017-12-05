df99d64808f4ac9a66b3663532b73a3d5c95c343
Yuki Sumida <y.sumida.0201@gmail.com>
2017-12-05 11:10:11 +0900

    String型で値を指定しない場合のrawValue

---
+++ StringEnumRawValue.playground/Contents.swift
@@ -0,0 +1,9 @@
+//: Playground - noun: a place where people can play
+
+enum Minions: String {
+    case kevin, bob, stuart
+}
+
+print(Minions.kevin.rawValue) // kevin
+print(Minions.bob.rawValue) // bob
+print(Minions.stuart.rawValue) // stuart
