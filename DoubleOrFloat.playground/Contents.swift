b340a752fe773919f86d6f249e855ac56f00ad68
Yuki Sumida <y.sumida.0201@gmail.com>
2017-12-27 21:06:47 +0900

    Stringへのキャスト

--- DoubleOrFloat.playground/Contents.swift
+++ DoubleOrFloat.playground/Contents.swift
@@ -6,4 +6,7 @@ let floatNum: Float = Float(5.02) * Float(100.0) // 502
 
 NSDecimalNumber(value: doubleNum) // 501.9999999999998
 NSDecimalNumber(value: floatNum) // 502
-
+String(doubleNum) // 502.0
+String(floatNum) // 502.0
+NSDecimalNumber(string: String(doubleNum)) // 502
+NSDecimalNumber(string: String(floatNum)) //502
